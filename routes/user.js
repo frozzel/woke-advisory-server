77ff87ead729ac717315f4b5df6837b70ecd84eb
Dennis Hickox <frozzel@me.com>
2023-06-23 11:28:13 -0400

    building out user profile

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { create, verifyEmail, resendEmailVerificationToken, forgetPassword, sendResetPasswordTokenStatus, resetPassword, signIn } = require("../controllers/user");
+const { create, verifyEmail, resendEmailVerificationToken, forgetPassword, sendResetPasswordTokenStatus, resetPassword, signIn, userInfo } = require("../controllers/user");
 const { isValidPassResetToken } = require("../utils/user");
 const { userValidator, validate, validatePassword, signInValidator, isAuth } = require("../utils/auth");
-const { sendError } = require("../utils/helper");
+
 
 const router = express.Router();
 
@@ -18,5 +18,7 @@ router.get('/is-auth', isAuth, (req, res) => {
   const {user} = req;
   res.json({user: {id: user._id, email: user.email, name: user.name, isVerified: user.isVerified, role: user.role}})
 })
+
+router.get("/profile/:userId", isAuth, userInfo)
 
 module.exports = router;
