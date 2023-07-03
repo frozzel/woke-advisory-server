8432dee8ba1544ab93aabf9cc1b670d21bb091c5
Dennis Hickox <frozzel@me.com>
2023-07-03 17:45:28 -0400

    added model for scraping images from unsplash

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getNews, getNewsMongo } = require("../controllers/news");
+const { getNews, getNewsMongo, getImages, getImagesMongo } = require("../controllers/news");
 
 const router = express.Router();
 
 router.get("/all", getNews);
 router.get("/relevant", getNewsMongo);
+router.get("/images/:searchTerm", getImages);
+router.get("/images", getImagesMongo);
 
 
 module.exports = router;
