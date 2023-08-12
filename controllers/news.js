756f3b66d5d35e01ccaf72d482a7f74b4902287e
Dennis Hickox <frozzel@me.com>
2023-08-12 13:52:32 -0400

    news

--- controllers/news.js
+++ controllers/news.js
@@ -15,7 +15,7 @@ exports.getNews = async (req, res) => {
         }
       };
     try{
-      const response = await fetch('https://newsapi.org/v2/everything?q=school+transgender&searchIn=title&from=2023-07-13&to=2023-07-20&sortBy=popularity', options);
+      const response = await fetch('https://newsapi.org/v2/everything?q=school+transgender&searchIn=title&from=2023-07-20&to=2023-08-12&sortBy=popularity', options);
     
       const news = await response.json();
       const mapNews = async (n) => {
